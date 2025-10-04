c0f208076cc0c28f9272dbf01a68debb8dc1b1e8
NasirAhmad <G00419630@atu.ie>
2025-10-04 01:53:31 +0100

    added test for put and parametrized age

--- tests/test_user.py
+++ tests/test_user.py
@@ -25,3 +25,16 @@ def test_get_user_404(client):
     r = client.get("/api/users/999")
     assert r.status_code == 404
 
+def test_put_user_404(client):
+    r = client.put("/api/users/999", json=user_payload(name="John"))
+    assert r.status_code == 404
+
+def test_put_user_ok(client):
+    r = client.put("/api/users/1", json=user_payload(name="Nasir"))
+    assert r.status_code == 200
+
+@pytest.mark.parametrize("invalid_age",[1,2,3,4,5])
+def test_invalid_age(client,invalid_age):
+    r = client.post("/api/users",json=user_payload(uid=4,age=invalid_age))
+    assert r.status_code == 422
+    
